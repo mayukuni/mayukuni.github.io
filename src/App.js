a7c5c439c92c7411b60fa63510bb96070379f53d
mayukuni <mayuukuni@gmail.com>
2023-07-28 20:50:16 -0300

    ux

--- src/App.js
+++ src/App.js
@@ -14,8 +14,11 @@ function App() {
           <a href="" className='hover:bg-red-300'>Onde me encontrar</a>
         </div>
       </header>
+        <a href="https://docs.google.com/presentation/d/13iMcvXSs9cB6t7LevXNldRb3s7lM-1xlT2qDi70sBEY/edit?usp=sharing" className='hover:bg-red-300 text-2xl bg-red-300'>Link para o estudo de caso do curso de UX Design. Estarei atualizando em breve.</a>
+        <br>
+        </br><a href="https://www.figma.com/proto/QWtFPKLMy1pQokhJze6bpm/Untitled?type=design&node-id=1-5&t=THhdySkur0GqWQUY-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A5&mode=design" className='hover:bg-red-300 text-2xl bg-red-300'>Link para o protótipo de alta fidelidade. Estarei atualizando em breve.</a>
       <div className="text-lg flex place-content-between">
-        <h1 className='flex self-center bg-red-300'>Hi! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis massa non magna laoreet, ut tristique velit pharetra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam vitae sodales lacus. Phasellus ultrices in elit eget pellentesque. Nam eu sollicitudin lorem. Fusce quis ultrices nunc, quis auctor justo. </h1>
+        {/* <h1 className='flex self-center bg-red-300'>Hi! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis massa non magna laoreet, ut tristique velit pharetra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam vitae sodales lacus. Phasellus ultrices in elit eget pellentesque. Nam eu sollicitudin lorem. Fusce quis ultrices nunc, quis auctor justo. </h1> */}
         <img src={photo} id="photo" className='w-80 rounded-full border-dotted border-4 border-red-300 hover:bg-red-300'/>
       </div>
       <div className='flex space-x-4 justify-center'>
