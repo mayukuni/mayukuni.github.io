c1743754246f46fc6a44bfdb49d86e68b58034f1
Mayumi Kuniyoshi <cinthya.m.kuniyoshi@gmail.com>
2024-05-09 00:00:26 -0300

    estilização e props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-export default function Header() {
+export default function Header(props) {
+  const { home, projects, findme } = props;
   return(
-    <header className="text-base flex place-content-between font-montserrat">
-      <h1 className="font-mono hover:bg-[#9288F8] text-5xl p-2 text-[#FFD2D7] font-montserrat"> Cinthya Mayumi Kuniyoshi</h1>
+    <header className="fixed w-full">
+      <div className=" bg-[#322653] flex place-content-between font-montserrat">
+        <h1 className="font-mono hover:bg-[#9288F8] text-5xl p-2 text-[#FFD2D7] font-montserrat"> Cinthya Mayumi Kuniyoshi</h1>
       <div className='flex self-end  space-x-4 text-4xl'>
-        <a href="" className='hover:bg-[#9288F8] p-2 text-[#FFD2D7]'>Sobre mim</a>
-        <a href="" className='hover:bg-[#9288F8] p-2 text-[#FFD2D7]'>Projetos</a>
-        <a href="" className='hover:bg-[#9288F8] p-2 text-[#FFD2D7]'>Onde me encontrar</a>
+        <a href={home} className='hover:bg-[#9288F8] p-2 text-[#FFD2D7]'>Sobre mim</a>
+        <a href={projects} className='hover:bg-[#9288F8] p-2 text-[#FFD2D7]'>Projetos</a>
+        <a href={findme} className='hover:bg-[#9288F8] p-2 text-[#FFD2D7]'>Onde me encontrar</a>
       </div>
+        </div>
+      <hr className='border-[#9288F8]'/>
     </header>
   )
 }
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 export default function ProjectCard(props) {
-  return(
-    <div className="bg-[#FFD2D7] font-montserrat rounded-lg    ">
-      Hi! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis massa non magna laoreet, ut tristique velit pharetra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam vitae sodales lacus. Phasellus ultrices in elit eget pellentesque. Nam eu sollicitudin lorem. Fusce quis ultrices nunc, quis auctor justo.
+  const { title, image, page, code } = props;
+
+  return (
+    <div className="bg-[#FFD2D7] font-montserrat m-12 mx-20 border-2 border-[#FFD2D7] p-10 flex justify-between">
+      <div className="flex flex-col justify-between">
+        <h1 className="text-8xl text-[#322653]">{title}</h1>
+        <div className="flex flex-col">
+          <a href={page} className="text-xl text-[#322653]">Link do projeto</a>
+          <a href={code} className="text-xl text-[#322653]">Link do código do projeto</a>
+        </div>
+      </div>
+      <img src={image} alt={title} className="w-2/4  rounded-3xl border-4 border-double border-[#9288F8]"/>
     </div>
-  )
+  );
 }
